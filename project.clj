a477e5e23fd33bec5c943534b13fe7c82353a1ff
Takahiro Hozumi <fatrow@googlemail.com>
2015-01-29 03:33:48 +0900

    update a way to specify warn-on-reflection option

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Apache Commons Exec wrapper for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.commons/commons-exec "1.2"]]
-  :warn-on-reflection true)
+  :global-vars {*warn-on-reflection* true})
