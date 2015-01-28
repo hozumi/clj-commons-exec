b4299b0e1ed00918ca1d40811d305e733fb2f64d
Takahiro Hozumi <fatrow@googlemail.com>
2015-01-29 03:48:03 +0900

    v1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hozumi/clj-commons-exec "1.1.1"
+(defproject org.clojars.hozumi/clj-commons-exec "1.2.0"
   :description "Apache Commons Exec wrapper for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.commons/commons-exec "1.3"]]
