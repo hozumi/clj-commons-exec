c9d58beb4bdc909872b31641fa89bc29821c0e42
Takahiro Hozumi <fatrow@googlemail.com>
2015-01-29 03:47:09 +0900

    update commons-exec to v1.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.hozumi/clj-commons-exec "1.1.1"
   :description "Apache Commons Exec wrapper for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.commons/commons-exec "1.2"]]
+                 [org.apache.commons/commons-exec "1.3"]]
   :global-vars {*warn-on-reflection* true})
